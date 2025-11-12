f8ee7baffb2904c465ed3e7a50be1575f58d54ce
Amam Raushan <amanraushan0@gmail.com>
2025-11-12 13:20:44 +0530

    check commit

--- fs_folder/fsAsyncAwait.js
+++ fs_folder/fsAsyncAwait.js
@@ -3,6 +3,6 @@ fs.unlink(filepath, (err) => {
     if (err) 
         console.error(err);
     else{
-        console.log(`File ${filename} deleted successfully.`);
+       // console.log(`File ${filename} deleted successfully.`);
     }
 });

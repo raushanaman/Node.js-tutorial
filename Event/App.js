81ce03316f55beae58a3b5c4967ed9fb04a4e537
Amam Raushan <amanraushan0@gmail.com>
2025-11-13 17:12:38 +0530

    events

--- Event/App.js
+++ Event/App.js
@@ -3,14 +3,53 @@ const EventEmit = require("events");
 
 // create an instance of EventEmmiter
 
-const emmiterObject = new EventEmit();
+const emitObject = new EventEmit();
 
 // 1. Define an event listener (addListener)
 
-emmiterObject.on("greet", (username) =>{
-    console.log(`Hello ${username} this is node.js event module tutorial`);
-})
+// emitObject.on("greet", (username) =>{
+//     console.log(`Hello ${username} this is node.js event module tutorial`);
+// })
 
-// 2. Trigger (emit) the "greet" event
+const eventCounts = {
+    "login": 0,
+    "purchase": 0,
+    "update": 0,
+    "Logout" : 0
+};
 
-emmiterObject.emit("greet", "aman");
+emitObject.on("user-login",
+    (username) => {
+        eventCounts.login += 1;
+        console.log(`Welcome! ${username} you successfully login.`);
+    }
+)
+
+emitObject.on("purchase", (username, product) => {
+    eventCounts.purchase += 1;
+    console.log(`Congratulations ${username} your order ${product} is successfully placed.`);
+ });
+
+ emitObject.on("update", (username, lastname) => {
+    eventCounts.update += 1;
+    console.log(`${username} your profile is successfully updated to ${username} ${lastname}`);
+ })
+
+ emitObject.on("logout", (username) => {
+    eventCounts.Logout += 1;
+    console.log(`${username} your are successfully logged out`);
+ })
+
+ emitObject.on("summary", () => {
+    console.log(eventCounts);
+ })
+
+// 2. Trigger (emit) the "greet" event 
+
+// emmiterObject.emit("greet", "aman");
+
+emitObject.emit("user-login", "aman");
+emitObject.emit("purchase", "aman" , "laptop");
+emitObject.emit("update", "aman", "Raushan");
+emitObject.emit("logout", "aman")
+emitObject.emit("summary");
